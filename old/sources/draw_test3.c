12f4f67fe21233c95b38943ed1467db10216c9e5
Soufiane <soufiane.gouiferda@gmail.com>
2020-04-23 10:55:34 +0000

    updated

--- old/sources/draw_test3.c
+++ old/sources/draw_test3.c
@@ -18,19 +18,21 @@ void draw_test3(t_rtv *rtv)
 {
 
     init_draw(rtv);
-
-    int final_color = BLACK;
-
-
+    int final_color = BLUE;
     int x = 0;
     int y = 0;
+
 
     while (x < rtv->screen_w)
     {
         y = 0;
         while (y < rtv->screen_h)
         {
-            final_color = BLACK;
+            int r = 255.99*(y / rtv->screen_h);
+            int g = 255.99*(x / rtv->screen_w);
+            int b = 255.99*(0.2);
+            
+            final_color = get_color1(r,g,b);
 
 
             add_px(rtv, x, y, final_color);
@@ -39,7 +41,6 @@ void draw_test3(t_rtv *rtv)
         }
         x++;
     }
-
     mlx_put_image_to_window(rtv->mlx, rtv->win, rtv->img_ptr, 0, 0);
-    draw_axis(rtv);
+    //draw_axis(rtv);
 }
