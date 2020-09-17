273f4d9b8f4288b3eb88265a2b0369e09709174a
Soufiane <soufiane.gouiferda@gmail.com>
2020-09-17 02:42:06 +0100

    updated

--- unit_tests/cylinder4.c
+++ unit_tests/cylinder4.c
@@ -1,63 +1,56 @@
 #include "headers.h"
 
-
-double			*hit_cylinder(t_ray *ray, t_shape *shape, double a, double b)
+double hit_cylinder(t_ray ray, t_figure shape)
 {
-	t_vector	oc;
-	double		*xs;
-	double		c;
-	double		disc;
+	t_vector oc;
+	double *xs;
+	double c;
+	double disc;
+	double a;
+	double b;
 
 	xs = (double *)malloc(sizeof(double) * 2);
-	xs[0] = MAX_I;
-	xs[1] = MAX_I;
-	oc = ray->a - shape->center;
-	a = dot(ray->b, ray->b) - pow(dot(ray->b, shape->axis_v), 2);
-	b = 2 * (dot(ray->b, oc) - (dot(ray->b, shape->axis_v) *
-		dot(oc, shape->axis_v)));
-	c = dot(oc, oc) - pow(dot(oc, shape->axis_v), 2) -
-	shape->radius * shape->radius;
+	xs[0] = 1000;
+	xs[1] = 1000;
+	oc = vectSub(ray.pos, shape.pos);
+	a = vectDot(ray.dir, ray.dir) - pow(vectDot(ray.dir, shape.dir), 2);
+	b = 2 * (vectDot(ray.dir, oc) - (vectDot(ray.dir, shape.dir) *
+									 vectDot(oc, shape.dir)));
+	c = vectDot(oc, oc) - pow(vectDot(oc, shape.dir), 2) -
+		shape.radius * shape.radius;
 	disc = b * b - 4 * a * c;
 	if (disc >= 0)
 	{
 		xs[0] = (-b + sqrt(disc)) / (2.0 * a);
 		xs[1] = (-b - sqrt(disc)) / (2.0 * a);
 	}
-	return (xs);
+	return (xs[0]);
 }
-
-
-
-
 
 int main()
 {
-    
-    // repere = screen (start and finish)
-    
 
-    //ray only
-    t_ray ray;
-    int z_range = 1000;
-    ray.pos = newVect(0,0,-z_range);
-    ray.dir = newVect(0,0,z_range);
+	// repere = screen (start and finish)
 
+	//ray only
+	t_ray ray;
+	int z_range = 1000;
+	ray.pos = newVect(0, 0, -z_range);
+	ray.dir = newVect(0, 0, z_range);
 
-    //object 
-    t_figure obj;
-    obj.pos = newVect(300,300,300);
-    obj.radius = 100;
-    obj.type = CYLINDER; // (cylinder / con / plane ..)
-    obj.dir = vectSub(newVect(300,300,300),obj.pos); //direction 
-	
+	//object
+	t_figure obj;
+	obj.pos = newVect(0, 0,0);
+	obj.radius = 100;
+	obj.type = CYLINDER;								// (cylinder / con / plane ..)
+	obj.dir = vectSub(newVect(10, 10, 300),obj.pos); //direction
 
-    //intersection
-    t_vector p_i;
-    float is_inter;
-    is_inter = cylinder_intersection(ray,obj);
-	printf("is_intersected: %.2f \n",is_inter);
+	//intersection
+	t_vector p_i;
+	double is_inter;
+	is_inter = hit_cylinder(ray, obj);
+	printf("is_intersected: %.2f \n", is_inter);
 	//printf("is_intersected: %d, point(x,y,z): (%.f,%.f,%.f) \n",is_inter, p_i.x, p_i.y,p_i.z);
-
 
 	return 0;
 }
--- unit_tests/headers.h
+++ unit_tests/headers.h
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 
 # define PLANE 1
