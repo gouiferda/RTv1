e3480032908eab5eb48fb6b5a84e0838f758dfb4
Soufiane <soufiane.gouiferda@gmail.com>
2020-09-17 08:40:37 +0100

    updated

--- unit_tests/example_intersection.c
+++ unit_tests/example_intersection.c
@@ -1,5 +1,5 @@
-
 #include "headers.h"
+
 
 
 
@@ -7,11 +7,35 @@
 int main()
 {
     
+    // repere = screen (start and finish)
     
-    t_vector v ;
-    v = newVect(1,1,1);
 
-    printf("v.x: %.f\n",v.x);
+    //ray only
+    t_ray ray;
+    int z_range = 1000;
+    ray.pos = newVect(0,0,-z_range);
+    ray.dir = newVect(0,0,z_range);
+
+
+    //object 
+    t_figure s;
+    s.pos = newVect(0,0,0);
+    s.radius = 100;
+    s.color = 125678;
+    s.type = SPHERE; // (cylinder / con / plane ..)
+    //s.dir = ... //direction 
+
+
+    //intersection
+    t_vector p_i;
+    int is_inter = 0;
+
+    // is_inter = sphere_ray_inter(ray,s,&p_i);
+  
+
+
+    // printf("is_intersected: %d, point(x,y,z): (%.f,%.f,%.f) \n",is_inter, p_i.x, p_i.y,p_i.z);
+
 
     return 0;
 }
